081a9af95d8d1a970f850c3e022d606f9230f2d4
godmuay <apinyakasi19@gmail.com>
2023-03-03 15:24:56 +0700

    lab23-f

--- average.cpp
+++ average.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char *argv[]){
+    double sum=0;
+    if(argc <2){
+        cout<<"Please input numbers to find average.";
+    }else{
+        for(int i=1; i<argc;i+=1){
+            sum+=atof(argv[i]);
+        }
+    cout << "---------------------------------\n";
+    cout << "Average of " << argc-1 << " numbers = " << sum/(argc-1) << endl;
+    cout << "---------------------------------\n";
+    }
+} 
